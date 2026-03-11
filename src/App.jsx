6516425f339a2e68980c7e8beb99d5f8c3c149db
jamesosei-hub <jamesoseiamaniampongagyemang@gmail.com>
2026-03-11 17:32:10 +0000

    fix netlify routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AssetDetail from "./pages/AssetDetail.jsx";
 import Explore from "./pages/Explore.jsx";
 import Home from "./pages/Home.jsx";
@@ -9,16 +9,14 @@ import SignUp from "./pages/SignUp.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/asset/:id" element={<AssetDetail />} />
-        <Route path="/learn" element={<Learn />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/asset/:id" element={<AssetDetail />} />
+      <Route path="/learn" element={<Learn />} />
+    </Routes>
   );
 }
 
